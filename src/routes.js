0d465da160fced887a7e96a89f6c26701e8b2489
Paulo Henrique Palmeira Franco <paulohpfranco@uol.com.br>
2019-11-14 00:13:24 -0300

    Conclusão do modulo 3

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import AvaliableController from './app/controllers/AvaliableController';
 
 import authMiddleware from './app/middlewares/auth';
 import NotificationController from './app/controllers/NotificationController';
@@ -23,12 +24,16 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvaliableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
